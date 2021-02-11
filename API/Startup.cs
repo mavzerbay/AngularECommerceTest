c630f30cb24028cedd9300ba4532fb4f442740b3
mavzerbay <mavzerbay@gmail.com>
2021-02-11 23:22:42 +0300

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -21,6 +22,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IProductRepository,ProductRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseMySql(_configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version(8, 0, 21))));
         }
