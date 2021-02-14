05f734737fcbf75e3ccf5cc2e6c35f060086848c
mavzerbay <mavzerbay@gmail.com>
2021-02-14 12:25:38 +0300

    End of section 9

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PagerComponent } from './components/pager/pager.component';
+
+
+
+@NgModule({
+  declarations: [PagingHeaderComponent, PagerComponent],
+  imports: [
+    CommonModule,
+    NgbPaginationModule,
+  ],
+  exports: [
+    NgbPaginationModule,
+    PagingHeaderComponent,
+    PagerComponent,
+  ]
+})
+export class SharedModule { }
